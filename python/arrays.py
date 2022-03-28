51850bf3a6270d7060b8c17ffee08348eddf4cda
Tresillocrack <tresillo2017@gmail.com>
2022-03-28 10:36:32 +0200

    arrays.py

--- python/arrays.py
+++ python/arrays.py
@@ -19,6 +19,26 @@ preguntas = [
     "¿Qué país tiene más copas del mundo de fútbol?",
     "¿De qué país es Lionel Andrés Messi?",
     "¿Dónde jugó en la mayoría de su carrera Kobe Bryant?",
+    "¿Qué equipo de fútbol tiene más Champions?",
+    "¿En qué continente se encuentra Suiza?",
+    "¿Cómo se llama el nuevo virus que nos ha tenido en pandemia durante 2 años?",
+    "¿Cuál era la moneda utilizada en España antes del Euro?",
+    "¿Cuál es la película más taquillera de la historia?",
+    "¿En qué año ganó España el mundial?",
+    "¿En qué continente se encuentra China?",
+    "¿Qué idioma es el más utilizado en el planeta?",
+    "¿Cómo se llamaba la enamorada de Don Quijote?",
+    "¿En qué serie sale el Demogorgon?",
+    "¿En qué ciudad se encuentra el Camp Nou?",
+    "¿Cómo se llama el rey de España?",
+    "¿Qué país está entre Perú y Colombia?",
+    "¿Quién fue la ganadora de los Grammy latinos en 2018?",
+    "¿Qué mide en París 333 metros?",
+    "¿Cuál es el pájaro símbolo de La Paz?",
+    "¿Cuántos jugadores tiene un equipo de voleibol?",
+    "¿Qué mes tiene menos días?",
+    "¿Quién fue el ganador de MotoGP en 2017?",
+    "¿Qué planeta se encuentra más cerca del Sol?"
 ]
 
 ans = [  
@@ -41,7 +61,27 @@ ans = [
     2, # pregunta 17
     2, # pregunta 18
     2, # pregunta 19
-    2  # pregunta 20
+    2, # pregunta 20
+    1, # pregunta 21
+    2, # pregunta 22
+    2, # pregunta 23
+    2, # pregunta 24
+    2, # pregunta 25
+    2, # pregunta 26
+    2, # pregunta 27
+    2, # pregunta 28
+    1, # pregunta 29
+    2, # pregunta 30
+    2, # pregunta 31
+    2, # pregunta 32
+    2, # pregunta 33
+    1, # pregunta 34
+    2, # pregunta 35
+    2, # pregunta 36
+    2, # pregunta 37
+    2, # pregunta 38
+    2, # pregunta 39
+    2, # pregunta 40
 ]  
 
 opcionesa = [     
@@ -69,7 +109,7 @@ opcionesa = [
 
     "Romulo y Remo",
 
-    "Don Quijote",
+    "Don Quijot ",
 
     "Freddy Mercury",
 
@@ -82,8 +122,48 @@ opcionesa = [
     "Francia",
 
     "Francia",
-    
+
     "Barcelona",
+
+    "Real Madrid",
+
+    "América",
+    
+    "Estoma Virus",
+    
+    "Libra",
+    
+    "Peppa Pig",
+    
+    "2002",
+    
+    "Europa",
+    
+    "Español",
+    
+    "Dulcinea",
+    
+    "Vikingos",
+    
+    "Valencia",
+    
+    "Guillermo II",
+    
+    "Alemania",
+    
+    "Rosalia",
+    
+    "La Mona Lisa",
+    
+    "Pajaro Carpintero",
+    
+    "20",
+    
+    "Octubre",
+    
+    "Cristiano Ronaldo",
+    
+    "Pluton"
 ]
 
 opcionesb = [
@@ -104,26 +184,66 @@ opcionesb = [
     "Abraham Mateo",
     
     "Teruel",
+
+    "Barcelona",
+
+    "Duero",
+
+    "Pomulo y Rema",
+
+    "Miguel de Cervantes",
+
+    "Jhon Lennon",
+
+    "Djmariio",
+
+    "Beethoven",
+
+    "Galileo",
+
+    "Brasil",
+
+    "Argentina",
+
+    "Lakers",
+
+    "Alcoyano",
+    
+    "Europa",
+    
+    "Coronavirus",
+    
+    "Peseta",
+    
+    "Avengers:Endgame",
+    
+    "2010",
+    
+    "Asia",
+    
+    "Mandarin",
+    
+    "Eustaquia",
+    
+    "Stranger Things",
     
     "Barcelona",
     
-    "Duero",
+    "Felipe VI",
     
-    "Pomulo y Rema",
+    "Ecuador",
     
-    "Miguel de Cervantes",
+    "Anita",
     
-    "Jhon Lennon",
+    "La Torre Eiffel",
     
-    "Djmariio",
+    "Paloma",
     
-    "Beethoven",
+    "6",
     
-    "Galileo",
+    "Febrero",
     
-    "Brasil",
+    "Marc Marquez",
     
-    "Argentina",
-    
-    "Lakers",
+    "Mercurio",
 ]
